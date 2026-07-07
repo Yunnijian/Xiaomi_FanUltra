fca3b57b0f1b8b6adb0d3beef6799554c96e9f43
Yunnijian <162805361+Yunnijian@users.noreply.github.com>
2026-07-07 18:40:32 +0800

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.mifan.kt"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
